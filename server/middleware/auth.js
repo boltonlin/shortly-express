2a25d848f60d8f0ddbcfa92c09fd139353607409
boltonlin <boltonlinbo@gmail.com>
2023-01-15 23:14:44 -0800

    passes Express Middleware tests

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,62 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const _ = require('lodash');
 
 module.exports.createSession = (req, res, next) => {
+  if (_.isEmpty(req.cookies)) {
+    // no cookies, create session
+    _makeNewSessionAndCookies(req, res)
+      .then(next);
+  } else {
+    // there are cookies, search sessions
+    models.Sessions.get({ hash: req.cookies.shortlyid })
+      .then(result => {
+        if (result) {
+          // there's a session associated with that cookie
+          const { hash, userId } = result;
+          req.session = { hash };
+          if (userId) {
+            // there's also a user associated with that session
+            return models.Users.get({ id: userId });
+          } else {
+            // valid cookies, but there is no user
+            return result;
+          }
+        } else {
+          // Malicious cookie case
+          return null;
+        }
+      }).then((result) => {
+        if (result) {
+          if (result.username) {
+            // there's also a user associated with that session
+            const { username, id } = result;
+            _.extend(req.session, {
+              user: { username: username },
+              userId: id
+            });
+          } else {
+            // valid cookies, but there is no user
+            return null;
+          }
+        } else {
+          // Malicious cookie case
+          return _makeNewSessionAndCookies(req, res);
+        }
+      }).then(next);
+  }
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
 
+let _makeNewSessionAndCookies = function (req, res) {
+  return models.Sessions.create()
+  .then(result => ({ 'id': result.insertId }))
+  .then(options => models.Sessions.get(options))
+  .then(({ hash }) => {
+    req.session = { hash };
+    res.cookies['shortlyid'] = { value: hash };
+  });
+}
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  let cookie = req.headers.cookie;
+  if (!cookie) {
+    req.cookies = {};
+  } else {
+    let cookieObj = cookie.split('; ')
+      .map(parts => parts.split('='))
+      .reduce((container, parts) => {
+        container[parts[0]] = parts[1];
+        return container;
+      }, {});
+    req.cookies = cookieObj;
+  }
+  next();
 };
 
 module.exports = parseCookies;
